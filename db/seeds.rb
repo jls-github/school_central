0a9ff9e310538969d2a89b5cb5f7b8c7c52f4d97
github username <github email address>
2020-04-13 14:29:56 -0700

    Added teacher seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,16 @@
 
 Lecture.destroy_all
 Course.destroy_all
+Teacher.destroy_all
 
-Course.create(subject: "Biology", number: "BI-202", title: "Elements of Biology")
-Course.create(subject: "Chemistry", number: "CH-202", title: "Organic Chemistry")
-Course.create(subject: "Anatomy and Physiology", number: "AP-202", title: "Advanced A&P")
-Course.create(subject: "Public Speaking", number: "SP-102", title: "Basics of Public Speaking")
-Course.create(subject: "English", number: "EN-102", title: "Grammer and Composition")
+Teacher.create(name: "Henry McIlroy", bio: "I was the first teacher here! I teach science.")
+Teacher.create(name: "Sarah Johnson", bio: "I was the second teacher here! I teach English and Speech")
+
+Course.create(subject: "Biology", number: "BI-202", title: "Elements of Biology", teacher_id: Teacher.first.id)
+Course.create(subject: "Chemistry", number: "CH-202", title: "Organic Chemistry", teacher_id: Teacher.first.id)
+Course.create(subject: "Anatomy and Physiology", number: "AP-202", title: "Advanced A&P", teacher_id: Teacher.first.id)
+Course.create(subject: "Public Speaking", number: "SP-102", title: "Basics of Public Speaking", teacher_id: Teacher.second.id)
+Course.create(subject: "English", number: "EN-102", title: "Grammer and Composition", teacher_id: Teacher.second.id)
 
 Lecture.create(course_id: Course.fourth.id, title: "Expository Speeeches Pt 1", content: "This is the first part of how you give an expository speech", date: Date.new(2020, 02, 03))
 Lecture.create(course_id: Course.fourth.id, title: "Expository Speeeches Pt 2", content: "This is the second part of how you give an expository speech", date: Date.new(2020, 02, 05))
