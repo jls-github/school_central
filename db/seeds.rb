abde820202b1a8e7473ed43ee70ffc3855fc0079
jls-github <souzajohnlee@gmail.com>
2020-04-13 13:49:51 -0700

    Seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Course.destroy_all
+
+Course.create(subject: "Biology", number: "BI-202", title: "Elements of Biology")
+Course.create(subject: "Chemistry", number: "CH-202", title: "Organic Chemistry")
+Course.create(subject: "Anatomy and Physiology", number: "AP-202", title: "Advanced A&P")
+Course.create(subject: "Public Speaking", number: "SP-102", title: "Basics of Public Speaking")
+Course.create(subject: "English", number: "EN-102", title: "Grammer and Composition")
