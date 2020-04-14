b51a7e5ca2423630b96603a70e3207a91a6d84b0
github username <github email address>
2020-04-14 11:04:42 -0700

    Added new and edit functions for lectures

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -5,4 +5,46 @@ class LecturesController < ApplicationController
         @course = @lecture.course
     end
 
+    def new
+        if teacher_session
+            @lecture = Course.new
+            @courses = Teacher.courses_by_id(session[:id])
+        else
+            redirect_to courses_path #change this to dashboard once implemented
+        end
+    end
+
+    def create
+        @lecture = Lecture.new(lecture_params)
+        if @lecture.save
+            redirect_to @lecture
+        else
+            render @lecture #need to add an error message here once validations are implemented
+        end
+    end
+
+    def edit
+        if teacher_session
+            @lecture = Lecture.find(params[:id])
+            @courses = Teacher.courses_by_id(session[:id])
+        else
+            redirect_to courses_path #change this tod ashboard once implemented
+        end
+    end
+
+    def update
+        @lecture = Lecture.find(params[:id])
+        if @lecture.update(lecture_params)
+            redirect_to @lecture
+        else
+            render @lecture #need to add an error message here once validations are implemented
+        end
+    end
+
+    private
+
+    def lecture_params
+        params.require(:lecture).permit(:title, content, date, course_id)
+    end
+
 end
