047d8946477a2af831dd80a0be7625e25481409a
jls-github <souzajohnlee@gmail.com>
2020-04-14 11:04:42 -0700

    Added new and edit functions for lectures

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :teachers, only: [:show, :index]
-  resources :lectures, only: [:show]
+  resources :lectures, only: [:show, :new, :create, :edit, :update]
   resources :courses, only: [:index, :show, :new, :create, :edit, :update]
 
   get '/login', to: "sessions#new", as: "login"
