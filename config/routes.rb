913beafba1d86dcadc7f32c960b48154b4915c74
jls-github <souzajohnlee@gmail.com>
2020-04-14 11:23:38 -0700

    Implemented delete action for lecture and course

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :teachers, only: [:show, :index]
-  resources :lectures, only: [:show, :new, :create, :edit, :update]
-  resources :courses, only: [:index, :show, :new, :create, :edit, :update]
+  resources :lectures, only: [:show, :new, :create, :edit, :update, :delete]
+  resources :courses
 
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create"
