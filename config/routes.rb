e889fd5a1dc6406295645d040bfb92a1f0ee3a83
jls-github <souzajohnlee@gmail.com>
2020-04-14 15:52:31 -0700

    Added signup page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   resources :teachers, only: [:show, :index]
   resources :lectures, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :courses
+  get '/dashboard', to: "static#dashboard", as: :dashboard
 
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create"
   get '/access_denied', to: "sessions#access_denied", as: :access_denied
   get '/permission_not_granted', to: "sessions#permission_not_granted", as: :permission_not_granted
-  get '/dashboard', to: "static#dashboard", as: :dashboard
+  get '/signup', to: "sessions#sign_up", as: :sign_up
+  post '/signup', to: "sessions#create_user"
 end
