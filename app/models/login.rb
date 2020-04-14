90888d73bbd11ee580ab75a901beca5bc133b0c9
github username <github email address>
2020-04-14 16:18:20 -0700

    Implemented account creation with nested fields for student creation

--- app/models/login.rb
+++ app/models/login.rb
@@ -2,5 +2,10 @@ class Login < ApplicationRecord
     has_secure_password
     has_one :student
     has_one :teacher
+    accepts_nested_attributes_for :student
 
+    def student_attributes=(student)
+        self.student = Student.create(name: student[:name], major: student[:major])
+        self.student.update(student)
+    end
 end
