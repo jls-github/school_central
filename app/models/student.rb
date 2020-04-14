a1ad1729706540e4d887c621a18998405b5266b1
jls-github <souzajohnlee@gmail.com>
2020-04-14 09:51:49 -0700

    Added authorization for course index page

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ApplicationRecord
     has_many :registrations
     has_many :courses, through: :registrations
     belongs_to :login
+
+    def self.courses_by_user_id(id)
+        Login.find(id).student.courses
+    end
 end
