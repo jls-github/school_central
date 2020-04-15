638766d0511ac6b0bdb05349c5d894568d75e966
github username <github email address>
2020-04-15 11:31:13 -0700

    Implemented method to show quiz grade

--- app/models/answer_submission.rb
+++ app/models/answer_submission.rb
@@ -1,4 +1,10 @@
 class AnswerSubmission < ApplicationRecord
   belongs_to :student
   belongs_to :answer
+
+  def quiz
+    self.answer.question.quiz
+  end
+
+  
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -7,4 +7,14 @@ class Student < ApplicationRecord
     def self.courses_by_id(id)
         Student.find(id).courses
     end
+
+    def taken_quizzes
+        self.answer_submissions.map {|submission| submission.quiz }.uniq
+    end
+
+    def percentage_for_quiz(quiz_id)
+        answers = Quiz.find(quiz_id).answers_from_student(self.id)
+        total_correct = answers.select{|answer| answer.correct == true }.length
+        ((total_correct.to_f / answers.length.to_f) * 100).to_i
+    end
 end
