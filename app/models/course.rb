a947945970d7f75a768594c0c9c93043e8050e85
github username <github email address>
2020-04-15 09:39:42 -0700

    First try at a quiz view page - will need tweaking

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,7 @@ class Course < ApplicationRecord
     has_many :registrations
     has_many :students, through: :registrations
     has_many :lectures
+    has_many :quizzes
     belongs_to :teacher
 
     
