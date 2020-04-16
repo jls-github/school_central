c3869f49703de8f401e19aa2b94f3143c8bcb10a
github username <github email address>
2020-04-15 17:13:55 -0700

    Fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+AnswerSubmission.destroy_all
+Answer.destroy_all
+Question.destroy_all
 Quiz.destroy_all
 Lecture.destroy_all
 Course.destroy_all
